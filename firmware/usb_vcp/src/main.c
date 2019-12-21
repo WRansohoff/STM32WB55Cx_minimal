a67ef279c0495147a4f1e8fc920f14f941482fe0
WRR <-@->
2019-12-20 23:47:29 -0500

    Add core project source/header files.

---
+++ firmware/usb_vcp/src/main.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * Reset handler: copy .data section and clear .bss section
+ * before jumping to the main loop.
+ */
+__attribute__( ( naked ) ) void reset_handler( void ) {
+  // Set the stack pointer to the 'end of stack' value.
+  __asm__( "LDR r0, =_estack\n\t"
+           "MOV sp, r0" );
+  // Branch to main.
+  __asm__( "B main" );
+}
+
+/**
+ * Main program.
+ */
+int main( void ) {
+  // Copy initialized data from .sidata (Flash) to .data (RAM)
+  memcpy( ( void* )&_sdata,
+          ( const void* )&_sidata,
+          ( &_edata - &_sdata ) );
+  // Clear the .bss section in RAM.
+  memset( &_sbss, 0x00, ( &_ebss - &_sbss ) );
+
+  // Enable GPIOA/B/C/E, USB, CRS peripherals.
+  RCC->AHB2ENR   |= ( RCC_AHB2ENR_GPIOAEN |
+                      RCC_AHB2ENR_GPIOBEN |
+                      RCC_AHB2ENR_GPIOCEN |
+                      RCC_AHB2ENR_GPIOEEN );
+  RCC->APB1ENR1  |= ( RCC_APB1ENR1_CRSEN |
+                      RCC_APB1ENR1_USBEN );
+
+  // Initial clock setup.
+  clock_setup();
+
+  // Output type: Push-pull, low-speed.
+  PoLED->MODER    &= ~(0x3 << (PiLED * 2));
+  PoLED->MODER    |=  (0x1 << (PiLED * 2));
+  PoLED->OTYPER   &= ~(0x1 << PiLED);
+  PoLED->OSPEEDR  &= ~(0x3 << (PiLED * 2));
+  PoLED->OSPEEDR  |=  (0x1 << (PiLED * 2));
+  PoLED->ODR      |=  (1 << PiLED);
+
+  // Toggle the LED every second.
+  while (1) {
+    delay_ms( 1000 );
+    PoLED->ODR ^=  (1 << PiLED);
+  }
+}
---
+++ firmware/usb_vcp/src/main.h
@@ -0,0 +1,10 @@
+#ifndef _VVC_MAIN_H
+#define _VVC_MAIN_H
+
+// Project includes.
+#include "global.h"
+
+// Pre-defined memory locations for program initialization.
+extern uint32_t _sidata, _sdata, _edata, _sbss, _ebss;
+
+#endif
